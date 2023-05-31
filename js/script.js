a47ccff1d8d4bc98f39e414946d19d02e0731125
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-31 15:41:16 +0200

    mails base done(?)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 console.log("JSOK");
-const mails = [
+const emails = [
   "gianni@yahoo.com",
   "genoveffo@yahoo.com",
   "ciccio@yahoo.com",
   "pippo@yahoo.com",
 ];
-console.table(mails);
+console.table(emails);
 let email = prompt("qual'è la tua email?", "").trim();
 console.log(email);
+for (let i = 0; i < email.length; i++) {
+  if (email === emails[0]) {
+    console.log("benvenuto utente");
+  } else if (email === emails[1]) {
+    console.log("benvenuto utente");
+  } else if (email === emails[2]) {
+    console.log("benvenuto utente");
+  } else if (email === emails[3]) {
+    console.log("benvenuto utente");
+  } else {
+    console.log("mi dispiace informazioni fornite non valide");
+  }
+}
