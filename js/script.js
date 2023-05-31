7a44dba4203b19f583a23e8fc3f23a04a220f1ee
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-05-31 15:18:39 +0200

    phase 0?

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("JSOK");
+const mails = [
+  "gianni@yahoo.com",
+  "genoveffo@yahoo.com",
+  "ciccio@yahoo.com",
+  "pippo@yahoo.com",
+];
+console.table(mails);
+let email = prompt("qual'è la tua email?", "").trim();
+console.log(email);
